1b5817dfda0efe458b916c8f3f37ee346678c5ee
Mishad <mishadcs2002@gmail.com>
2025-02-05 22:36:04 +0600

    Button, OnClick, Toast,  Box, Icons and DropDownMenus, Modifier, VerticalArrangement and HorizontalAlignment, Spacer vs Padding,Adding DropdownMenus and DropDownMenuItems

--- 7_unit_converter_app/app/src/main/java/com/example/a7_unit_converter_app/MainActivity.kt
+++ 7_unit_converter_app/app/src/main/java/com/example/a7_unit_converter_app/MainActivity.kt
@@ -1,24 +1,42 @@
 package com.example.a7_unit_converter_app
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.a7_unit_converter_app.ui.theme._7_unit_converter_appTheme
+
 /*class MainActivity : ComponentActivity() {
     // MainActivity inherits from ComponentActivity, serving as the entry point of the app.
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,7 +75,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             _7_unit_converter_appTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Surface(modifier = Modifier.padding(innerPadding).fillMaxSize()) {
+                    Surface(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    ) {
                         UnitConverter()
                     }
                 }
@@ -67,15 +89,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun UnitConverter(){
-    Column {
-        Text("Unit Converter")
+fun UnitConverter() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Unit Converter",
+            color = Color.Blue,
+            style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange = {})
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
+            Box {
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = true, onDismissRequest = {}) {
+                    DropdownMenuItem(text = { Text("Centimeters ") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Meters ") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Feet ") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Millimeters ") }, onClick = {})
+                }
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box {
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = {}) {
+                    DropdownMenuItem(text = { Text("Centimeters ") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Meters ") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Feet ") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Millimeters ") }, onClick = {})
+                }
+            }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Text("Result")
     }
 }
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -86,6 +145,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun UnitConverterPreview(){
+fun UnitConverterPreview() {
     UnitConverter()
 }
